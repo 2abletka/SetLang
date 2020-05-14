53f0c586c90af31a228f37928a308311a4df1581
Aleksey Dvornichenko <leshadvornichenko@mail.ru>
2020-05-14 15:31:36 +0300

    Add Expressions and Tokens

---
+++ src/com/compiler/parser/sintaxtree/Expression.java
@@ -0,0 +1,7 @@
+package com.compiler.parser.sintaxtree;
+
+import main.java.com.vectorforce.compiler.vars.Value;
+
+public interface Expression {
+    Value evaluate();
+}
---
+++ src/com/compiler/parser/sintaxtree/VariableExpression.java
@@ -0,0 +1,27 @@
+package com.compiler.parser.sintaxtree;
+
+import main.java.com.vectorforce.compiler.vars.NumberValue;
+import main.java.com.vectorforce.compiler.vars.Value;
+import main.java.com.vectorforce.compiler.vars.Variables;
+
+public class VariableExpression implements Expression {
+    private final String name;
+
+    public VariableExpression(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Value evaluate() {
+        if(!Variables.isExists(name)){
+//            throw new RuntimeException("Constant does not exists");
+            Variables.set(name, new NumberValue(0));
+        }
+        return Variables.get(name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", name);
+    }
+}
